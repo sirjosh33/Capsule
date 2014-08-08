c07206ce4297da614efd9e970eee35782b68e2bf
Josh <joshuadkrueger@gmail.com>
2014-08-07 22:41:48 -0500

    8-6-14

---
+++ Capsule/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ion.Data.Linq;
+
+namespace Capsule.Data
+{
+    class DataContext : DbContext
+    {
+        public DataContext(string server, string database, string userID, string password)
+            : base(server, database, userID, password)
+        {
+           
+        }
+
+        public DataContext(string connectionString)
+            : base(connectionString)
+        {
+            
+        }
+    }
+}
---
+++ Capsule/Data/Tables/Contact.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using Ion.Data.Linq.Mapping;
+
+namespace Capsule.Data.Tables
+{
+    [Table(Name = "Contacts")]
+    public class Contact : DbTableEquatable<IDbTableEquatable<long>>, IDbTableEquatable<long>
+    {
+
+        public Contact()
+        {
+            /*
+            Zip = "";
+            DateEdited = DateTime.Now;
+            DateEntered = DateTime.Now;
+            AppointmentCount = 0;
+            IsLocked = false;
+            LockedByUserID = 0;
+              */
+        }
+
+        [Column(IsPrimaryKey = true, DbType = "Bigint NOT NULL IDENTITY", AutoSync = AutoSync.OnInsert, IsDbGenerated = true)]
+        public long ID { get; set; }
+        [Column]
+        public string FirstName { get; set; }
+        [Column]
+        public string LastName { get; set; }
+        [Column]
+        public int ContactTypeID { get; set; }
+        [Column]
+        public string Email { get; set; }
+
+    }
+}
