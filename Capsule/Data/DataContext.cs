4ab0148f32c8ad62c33349987a00e6f10d21686b
jdemeuse1204 <james.demeuse@gmail.com>
2014-08-07 22:55:44 -0500

    8/6/2014

--- Capsule/Data/DataContext.cs
+++ Capsule/Data/DataContext.cs
@@ -1,24 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Capsule.Data.Tables;
 using Ion.Data.Linq;
+using Ion.Data.Linq.Mapping;
 
 namespace Capsule.Data
 {
-    class DataContext : DbContext
-    {
-        public DataContext(string server, string database, string userID, string password)
-            : base(server, database, userID, password)
-        {
-           
-        }
+	public class DataContext : DbContext
+	{
+		#region Constructor
+		public DataContext(string server, string database, string userID, string password)
+			: base(server, database, userID, password)
+		{
+			_initialize();
+		}
 
-        public DataContext(string connectionString)
-            : base(connectionString)
-        {
-            
-        }
+		public DataContext(string connectionString)
+			: base(connectionString)
+		{
+			_initialize();
+		}
+
+		private void _initialize()
+		{
+			Contacts = GetTable<Contact>();
+		}
+		#endregion
+
+		#region Properties
+		public Table<Contact> Contacts { get; set; }
+		#endregion
+
+		#region Methods
+		public virtual void DeleteOnSave<TEntity>(TEntity entity)
+			where TEntity : DbTableEquatable<IDbTableEquatable<long>>
+		{
+			base.DeleteOnSave<TEntity, long>(entity);
+		}
+
+		public virtual void SaveChanges<TEntity>(TEntity entity)
+			where TEntity : DbTableEquatable<IDbTableEquatable<long>>
+		{
+			base.SaveChanges<TEntity, long>(entity);
+		}
+
+		public Table<TEntity> GetTable<TEntity>()
+			where TEntity : DbTableEquatable<IDbTableEquatable<long>>
+		{
+			return base.GetTable<TEntity, long>();
+		}
+
+		protected override void _preprocessSave<TEntity, TPKType>(TEntity entity)
+		{
+
+		}
+		#endregion
+
+		#region Disposable Methods
+		public void Dispose()
+		{
+			Contacts = null;
+
+			base.Dispose();
+		}
+		#endregion
     }
 }
